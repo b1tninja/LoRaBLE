0e33cf6604e8917d792926115eb6169f9e19e6da
Justin Capella <justincapella@gmail.com>
2021-08-01 18:31:17 -0700

    reformatted

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # LoRaBLE
 
-A low power Long-Range [LoRa](https://en.wikipedia.org/wiki/LoRa) Bluetooth ([BLE](https://en.wikipedia.org/wiki/Bluetooth_Low_Energy)) relay for the ESP32 LoRa v2.
-
+A low power Long-Range [LoRa](https://en.wikipedia.org/wiki/LoRa)
+Bluetooth ([BLE](https://en.wikipedia.org/wiki/Bluetooth_Low_Energy)) relay for the ESP32 LoRa v2.
 
 # Hardware
-The ESP32 LoRa 1-CH Gateway combines an ESP32 -- a **programmable** microcontroller featuring both WiFi and Bluetooth radios -- with an RFM95W LoRa transceiver to create a single-channel LoRa gateway. It's a perfect, low-cost tool for monitoring a dozen-or-so LoRa devices, and relaying their messages up to the cloud.
 
-[Amazon](https://www.amazon.com/MakerFocus-Development-Bluetooth-0-96inch-Display/dp/B076MSLFC9)
+The ESP32 LoRa 1-CH Gateway combines an ESP32 -- a **programmable** microcontroller featuring both WiFi and Bluetooth
+radios -- with an RFM95W LoRa transceiver to create a single-channel LoRa gateway. It's a perfect, low-cost tool for
+monitoring a dozen-or-so LoRa devices, and relaying their messages up to the cloud.
+
+[ESP32 LoRa 32 (V2), ESP32 Development Board](https://www.amazon.com/MakerFocus-Development-Bluetooth-0-96inch-Display/dp/B076MSLFC9)
+
 ![ESP32 LoRa V2](https://m.media-amazon.com/images/I/710x6RMB+YL._AC_SX679_.jpg)
 
-
 # Credits
+
 Derived from example code provided here:
 
- - https://github.com/HelTecAutomation/Heltec_ESP32/blob/master/examples/Factory_Test/WiFi_LoRa_32FactoryTest/WiFi_LoRa_32FactoryTest.ino
- - https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/examples/BLE_uart/BLE_uart.ino
+- https://github.com/HelTecAutomation/Heltec_ESP32/blob/master/examples/Factory_Test/WiFi_LoRa_32FactoryTest/WiFi_LoRa_32FactoryTest.ino
+- https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/examples/BLE_uart/BLE_uart.ino
--- lorable/lorable.ino
+++ lorable/lorable.ino
@@ -1,36 +1,13 @@
+#include "Arduino.h"
+#include "heltec.h"
+#include "WiFi.h"
+#include "logo.h"
 
-/*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in Ftthis example just a byte incremented every second. 
-*/
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-
-BLEServer *pServer = NULL;
-BLECharacteristic * pTxCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t txValue = 0;
 
 // Bluetooth defines
 #define SERVICE_NAME "LoRaBLE"
@@ -38,335 +15,312 @@ uint8_t txValue = 0;
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-
-#include "Arduino.h"
-#include "heltec.h"
-#include "WiFi.h"
-#include "images.h"
-
 #define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 
+BLEServer *pServer = NULL;
+BLECharacteristic *pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint8_t txValue = 0;
 String rssi = "RSSI --";
 String packSize = "--";
 String packet;
-
 unsigned int counter = 0;
-
 bool receiveflag = false; // software flag for LoRa receiver, received data makes it true.
-
 long lastSendTime = 0;        // last send time
 int interval = 1000;          // interval between sends
 uint64_t chipid;
 
-void logo(){
-	Heltec.display -> clear();
-	Heltec.display -> drawXbm(0,5,logo_width,logo_height,(const unsigned char *)logo_bits);
-	Heltec.display -> display();
+void logo() {
+    Heltec.display->clear();
+    Heltec.display->drawXbm(0, 5, logo_width, logo_height, (const unsigned char *) logo_bits);
+    Heltec.display->display();
 }
 
-void WIFISetUp(void)
-{
-	// Set WiFi to station mode and disconnect from an AP if it was previously connected
-	WiFi.disconnect(true);
-	delay(100);
-	WiFi.mode(WIFI_STA);
-	WiFi.setAutoConnect(true);
-	WiFi.begin("Your WiFi SSID","Your Password");//fill in "Your WiFi SSID","Your Password"
-	delay(100);
+void WIFISetUp(void) {
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.disconnect(true);
+    delay(100);
+    WiFi.mode(WIFI_STA);
+    WiFi.setAutoConnect(true);
+    WiFi.begin("Your WiFi SSID", "Your Password");//fill in "Your WiFi SSID","Your Password"
+    delay(100);
 
-	byte count = 0;
-	while(WiFi.status() != WL_CONNECTED && count < 10)
-	{
-		count ++;
-		delay(500);
-		Heltec.display -> drawString(0, 0, "Connecting...");
-		Heltec.display -> display();
-	}
+    byte count = 0;
+    while (WiFi.status() != WL_CONNECTED && count < 10) {
+        count++;
+        delay(500);
+        Heltec.display->drawString(0, 0, "Connecting...");
+        Heltec.display->display();
+    }
 
-	Heltec.display -> clear();
-	if(WiFi.status() == WL_CONNECTED)
-	{
-		Heltec.display -> drawString(0, 0, "Connecting...OK.");
-		Heltec.display -> display();
+    Heltec.display->clear();
+    if (WiFi.status() == WL_CONNECTED) {
+        Heltec.display->drawString(0, 0, "Connecting...OK.");
+        Heltec.display->display();
 //		delay(500);
-	}
-	else
-	{
-		Heltec.display -> clear();
-		Heltec.display -> drawString(0, 0, "Connecting...Failed");
-		Heltec.display -> display();
-		//while(1);
-	}
-	Heltec.display -> drawString(0, 10, "WIFI Setup done");
-	Heltec.display -> display();
-	delay(500);
+    } else {
+        Heltec.display->clear();
+        Heltec.display->drawString(0, 0, "Connecting...Failed");
+        Heltec.display->display();
+        //while(1);
+    }
+    Heltec.display->drawString(0, 10, "WIFI Setup done");
+    Heltec.display->display();
+    delay(500);
 }
 
-void WIFIScan(unsigned int value)
-{
-	unsigned int i;
+void WIFIScan(unsigned int value) {
+    unsigned int i;
     WiFi.mode(WIFI_STA);
 
-	for(i=0;i<value;i++)
-	{
-		Heltec.display -> drawString(0, 20, "Scan start...");
-		Heltec.display -> display();
+    for (i = 0; i < value; i++) {
+        Heltec.display->drawString(0, 20, "Scan start...");
+        Heltec.display->display();
 
-		int n = WiFi.scanNetworks();
-		Heltec.display -> drawString(0, 30, "Scan done");
-		Heltec.display -> display();
-		delay(500);
-		Heltec.display -> clear();
+        int n = WiFi.scanNetworks();
+        Heltec.display->drawString(0, 30, "Scan done");
+        Heltec.display->display();
+        delay(500);
+        Heltec.display->clear();
 
-		if (n == 0)
-		{
-			Heltec.display -> clear();
-			Heltec.display -> drawString(0, 0, "no network found");
-			Heltec.display -> display();
-			//while(1);
-		}
-		else
-		{
-			Heltec.display -> drawString(0, 0, (String)n);
-			Heltec.display -> drawString(14, 0, "networks found:");
-			Heltec.display -> display();
-			delay(500);
+        if (n == 0) {
+            Heltec.display->clear();
+            Heltec.display->drawString(0, 0, "no network found");
+            Heltec.display->display();
+            //while(1);
+        } else {
+            Heltec.display->drawString(0, 0, (String) n);
+            Heltec.display->drawString(14, 0, "networks found:");
+            Heltec.display->display();
+            delay(500);
 
-			for (int i = 0; i < n; ++i) {
-			// Print SSID and RSSI for each network found
-				Heltec.display -> drawString(0, (i+1)*9,(String)(i + 1));
-				Heltec.display -> drawString(6, (i+1)*9, ":");
-				Heltec.display -> drawString(12,(i+1)*9, (String)(WiFi.SSID(i)));
-				Heltec.display -> drawString(90,(i+1)*9, " (");
-				Heltec.display -> drawString(98,(i+1)*9, (String)(WiFi.RSSI(i)));
-				Heltec.display -> drawString(114,(i+1)*9, ")");
-				//            display.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-				delay(10);
-			}
-		}
+            for (int i = 0; i < n; ++i) {
+                // Print SSID and RSSI for each network found
+                Heltec.display->drawString(0, (i + 1) * 9, (String)(i + 1));
+                Heltec.display->drawString(6, (i + 1) * 9, ":");
+                Heltec.display->drawString(12, (i + 1) * 9, (String)(WiFi.SSID(i)));
+                Heltec.display->drawString(90, (i + 1) * 9, " (");
+                Heltec.display->drawString(98, (i + 1) * 9, (String)(WiFi.RSSI(i)));
+                Heltec.display->drawString(114, (i + 1) * 9, ")");
+                //            display.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+                delay(10);
+            }
+        }
 
-		Heltec.display -> display();
-		delay(800);
-		Heltec.display -> clear();
-	}
+        Heltec.display->display();
+        delay(800);
+        Heltec.display->clear();
+    }
 }
 
-bool resendflag=false;
-bool deepsleepflag=false;
-void interrupt_GPIO0()
-{
-  delay(10);
-  if(digitalRead(0)==0)
-  {
-      if(digitalRead(LED)==LOW)
-      {resendflag=true;}
-      else
-      {
-        deepsleepflag=true;
-      }     
-  }
+bool resendflag = false;
+bool deepsleepflag = false;
+
+void interrupt_GPIO0() {
+    delay(10);
+    if (digitalRead(0) == 0) {
+        if (digitalRead(LED) == LOW) { resendflag = true; }
+        else {
+            deepsleepflag = true;
+        }
+    }
 }
 
-void send()
-{
+void send() {
     LoRa.beginPacket();
     LoRa.print("hello ");
     LoRa.print(counter++);
     LoRa.endPacket();
 }
-void displaySendReceive()
-{
-    Heltec.display -> drawString(0, 50, "Packet " + (String)(counter-1) + " sent done");
-    Heltec.display -> drawString(0, 0, "Received Size  " + packSize + " packages:");
-    Heltec.display -> drawString(0, 10, packet);
-    Heltec.display -> drawString(0, 20, "With " + rssi + "db");
-    Heltec.display -> display();
+
+void displaySendReceive() {
+    Heltec.display->drawString(0, 50, "Packet " + (String)(counter - 1) + " sent done");
+    Heltec.display->drawString(0, 0, "Received Size  " + packSize + " packages:");
+    Heltec.display->drawString(0, 10, packet);
+    Heltec.display->drawString(0, 20, "With " + rssi + "db");
+    Heltec.display->display();
     delay(100);
-    Heltec.display -> clear();
+    Heltec.display->clear();
 }
+
 void onReceive(int packetSize)//LoRa receiver interrupt service
 {
-	//if (packetSize == 0) return;
+    //if (packetSize == 0) return;
 
-	packet = "";
-    packSize = String(packetSize,DEC);
+    packet = "";
+    packSize = String(packetSize, DEC);
 
-    while (LoRa.available())
-    {
-		packet += (char) LoRa.read();
+    while (LoRa.available()) {
+        packet += (char) LoRa.read();
     }
 
     Serial.println(packet);
-    rssi = "RSSI: " + String(LoRa.packetRssi(), DEC);    
-    receiveflag = true;    
+    rssi = "RSSI: " + String(LoRa.packetRssi(), DEC);
+    receiveflag = true;
 }
 
 
-
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) {
+        deviceConnected = true;
     };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
+    void onDisconnect(BLEServer *pServer) {
+        deviceConnected = false;
     }
 };
 
 
-class MyCallbacks: public BLECharacteristicCallbacks {
+class MyCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
+        std::string rxValue = pCharacteristic->getValue();
 
-      if (rxValue.length() > 0) {
-        Serial.println("*********");
-        Serial.print("Received Value: ");
-        for (int i = 0; i < rxValue.length(); i++)
-          Serial.print(rxValue[i]);
-        Serial.println();
-        Serial.println("*********");
+        if (rxValue.length() > 0) {
+            Serial.println("*********");
+            Serial.print("Received Value: ");
+            for (int i = 0; i < rxValue.length(); i++)
+                Serial.print(rxValue[i]);
+            Serial.println();
+            Serial.println("*********");
 
-        LoRa.beginPacket();
-        LoRa.print(rxValue.c_str());
-        LoRa.endPacket();
-      }
+            LoRa.beginPacket();
+            LoRa.print(rxValue.c_str());
+            LoRa.endPacket();
+        }
     }
 };
 
 
-void setup()
-{
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*LoRa Enable*/, true /*Serial Enable*/, true /*LoRa use PABOOST*/, BAND /*LoRa RF working band*/);
+void setup() {
+    Heltec.begin(true /*DisplayEnable Enable*/, true /*LoRa Enable*/, true /*Serial Enable*/, true /*LoRa use PABOOST*/,
+                 BAND /*LoRa RF working band*/);
 
-  logo();
-  delay(300);
-  Heltec.display -> clear();
+    logo();
+    delay(300);
+    Heltec.display->clear();
 
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // Create the BLE Device
-  BLEDevice::init(SERVICE_NAME);
+    // Create the BLE Device
+    BLEDevice::init(SERVICE_NAME);
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
-  pTxCharacteristic = pService->createCharacteristic(
-                    CHARACTERISTIC_UUID_TX,
-                    BLECharacteristic::PROPERTY_NOTIFY
-                  );
-                      
-  pTxCharacteristic->addDescriptor(new BLE2902());
+    // Create a BLE Characteristic
+    pTxCharacteristic = pService->createCharacteristic(
+            CHARACTERISTIC_UUID_TX,
+            BLECharacteristic::PROPERTY_NOTIFY
+    );
 
-  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
-                       CHARACTERISTIC_UUID_RX,
-                      BLECharacteristic::PROPERTY_WRITE
-                    );
+    pTxCharacteristic->addDescriptor(new BLE2902());
 
-  pRxCharacteristic->setCallbacks(new MyCallbacks());
+    BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
+            CHARACTERISTIC_UUID_RX,
+            BLECharacteristic::PROPERTY_WRITE
+    );
 
-  // TODO: battery level characteristic
-  //BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit characteristic UUID
-  //  BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
-  // For ayone who needs to get the battery level from a BLE device (if it supports such), the Battery Service UUID is 0x180F and the battery level characterisitc UUID is 0x2A19, reporting a one byte value with the battery level as a percentage (UInt8).
+    pRxCharacteristic->setCallbacks(new MyCallbacks());
 
-  // Start the service
-  pService->start();
+    // TODO: battery level characteristic
+    //BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit characteristic UUID
+    //  BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
+    // For ayone who needs to get the battery level from a BLE device (if it supports such), the Battery Service UUID is 0x180F and the battery level characterisitc UUID is 0x2A19, reporting a one byte value with the battery level as a percentage (UInt8).
 
-  // Start advertising
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
- 
-  /*
-  WIFISetUp();
-  WiFi.disconnect(); //重新初始化WIFI
-  WiFi.mode(WIFI_STA);
-  delay(100);
+    // Start the service
+    pService->start();
 
-  WIFIScan(1);
-  */
+    // Start advertising
+    pServer->getAdvertising()->start();
+    Serial.println("Waiting a client connection to notify...");
 
-  chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
-  Serial.printf("ESP32ChipID=%04X",(uint16_t)(chipid>>32));//print High 2 bytes
-  Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
+    /*
+    WIFISetUp();
+    WiFi.disconnect(); //重新初始化WIFI
+    WiFi.mode(WIFI_STA);
+    delay(100);
 
-  attachInterrupt(0,interrupt_GPIO0,FALLING);
+    WIFIScan(1);
+    */
 
-  LoRa.setSyncWord(0x23);
-   
-  LoRa.onReceive(onReceive);
+    chipid = ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+    Serial.printf("ESP32ChipID=%04X", (uint16_t)(chipid >> 32));//print High 2 bytes
+    Serial.printf("%08X\n", (uint32_t) chipid);//print Low 4bytes.
+
+    attachInterrupt(0, interrupt_GPIO0, FALLING);
+
+    LoRa.setSyncWord(0x23);
+
+    LoRa.onReceive(onReceive);
     send();
     LoRa.receive();
     displaySendReceive();
 }
 
 
-void loop()
-{
- if(deepsleepflag)
- {
-  LoRa.end();
-  LoRa.sleep();
-  delay(100);
-  pinMode(4,INPUT);
-  pinMode(5,INPUT);
-  pinMode(14,INPUT);
-  pinMode(15,INPUT);
-  pinMode(16,INPUT);
-  pinMode(17,INPUT);
-  pinMode(18,INPUT);
-  pinMode(19,INPUT);
-  pinMode(26,INPUT);
-  pinMode(27,INPUT);
-  digitalWrite(Vext,HIGH);
-  delay(2);
-  esp_deep_sleep_start();
- }
+void loop() {
+    if (deepsleepflag) {
+        LoRa.end();
+        LoRa.sleep();
+        delay(100);
+        pinMode(4, INPUT);
+        pinMode(5, INPUT);
+        pinMode(14, INPUT);
+        pinMode(15, INPUT);
+        pinMode(16, INPUT);
+        pinMode(17, INPUT);
+        pinMode(18, INPUT);
+        pinMode(19, INPUT);
+        pinMode(26, INPUT);
+        pinMode(27, INPUT);
+        digitalWrite(Vext, HIGH);
+        delay(2);
+        esp_deep_sleep_start();
+    }
 
- if(resendflag)
- {
-   resendflag=false;
-   send();      
-   LoRa.receive();
-   displaySendReceive();
- }
- if(receiveflag)
- {
-    digitalWrite(25,HIGH);
-    displaySendReceive();
-    delay(1000);
-    receiveflag = false;  
-    send();
-    LoRa.receive();
-    // TODO: relay over bluetooth
-    displaySendReceive();
-  }
+    if (resendflag) {
+        resendflag = false;
+        send();
+        LoRa.receive();
+        displaySendReceive();
+    }
+    if (receiveflag) {
+        digitalWrite(25, HIGH);
+        displaySendReceive();
+        delay(1000);
+        receiveflag = false;
+        send();
+        LoRa.receive();
+        // TODO: relay over bluetooth
+        displaySendReceive();
+    }
 
-  if (deviceConnected) {
-      pTxCharacteristic->setValue(&txValue, 1);
-      pTxCharacteristic->notify();
-      txValue++;
-      delay(10); // bluetooth stack will go into congestion, if too many packets are sent
-  }
+    if (deviceConnected) {
+        pTxCharacteristic->setValue(&txValue, 1);
+        pTxCharacteristic->notify();
+        txValue++;
+        delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+    }
 
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected) {
-      Serial.println("Disconnecting...");
-      delay(500); // give the bluetooth stack the chance to get things ready
-      pServer->startAdvertising(); // restart advertising
-      Serial.println("start advertising");
-      oldDeviceConnected = deviceConnected;
-  }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        Serial.println("Disconnecting...");
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
 
-  // connecting
-  if (deviceConnected && !oldDeviceConnected) {
-      Serial.println("Connecting...");
-      // do stuff here on connecting
-      oldDeviceConnected = deviceConnected;
-  }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        Serial.println("Connecting...");
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
